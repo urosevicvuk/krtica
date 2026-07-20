bc0cd3d3f81881ba514bdbb3a72313bdf95422d9
urosevicvuk <vuk23urosevic@gmail.com>
2026-07-21 00:04:06 +0200

    fix(taste): moved this to the new 1.25 example of workGroup goroutines
    Signed-off-by: urosevicvuk <vuk23urosevic@gmail.com>

--- internal/forward/forward.go
+++ internal/forward/forward.go
@@ -14,17 +14,18 @@ type halfCloser interface {
 // Splice splices two net.Conns
 func Splice(a, b net.Conn) {
 	var wg sync.WaitGroup
-	wg.Add(2)
-	go cp(&wg, a, b)
-	go cp(&wg, b, a)
+
+	wg.Go(func() { cp(a, b) })
+	wg.Go(func() { cp(b, a) })
+
 	wg.Wait()
+
 	_ = a.Close()
 	_ = b.Close()
 }
 
 // cp copies data between two net.Conns
-func cp(wg *sync.WaitGroup, dst, src net.Conn) {
-	defer wg.Done()
+func cp(dst, src net.Conn) {
 	_, _ = io.Copy(dst, src)
 	if hc, ok := dst.(halfCloser); ok {
 		_ = hc.CloseWrite()
